7b74a1c0e60688863cb3a4c98f66de58583c3130
Sourav Prasad <souravpd007@gmail.com>
2019-10-01 17:21:49 +0530

    Made changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ anime({
   opacity: 1,
   delay: anime.stagger(80)
 });
-
 var firstTime = true;
 var message = "Cool. You tried it!";
 var later = "\n\nThis is the real easter egg.\n\nThe first message isn't a real easter egg because the Commit Message says what to do.\n\nThis one, however, is a message revealed only if the number '3' is hovered over more than once.\n\n"
