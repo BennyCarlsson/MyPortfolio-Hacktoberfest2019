3e8aca4cfc010a2c1f7dd50b10e0088b46c3f6cd
Analise Burtet <35999782+analiseburtet@users.noreply.github.com>
2019-10-05 17:47:43 -0300

    add scroll to top button (#159)

--- app.js
+++ app.js
@@ -292,3 +292,19 @@ document.getElementById("circle-profile-img").addEventListener('click', (event) 
     BombIT()
   }
 })
+
+topButton = document.getElementById("goTopBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
