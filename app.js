955d33d6895dadbcb350be881a5cba0acd0b4930
CodinMaster <latheyapoorv@gmail.com>
2019-10-06 15:57:09 +0530

    Comic-Sansify the Webpage.

--- app.js
+++ app.js
@@ -308,3 +308,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function change_css(){
+  document.body.style.cssText = 'font-family: "Comic Sans MS", sans-serif !important; animation: bg-animate 8s linear infinite alternate both;';
+}
