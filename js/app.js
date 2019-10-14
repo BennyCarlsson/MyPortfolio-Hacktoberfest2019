707133f8485c4a4cf1baf66c75ea36cc2e011683
donishka <donishkat@gmail.com>
2019-10-14 23:02:24 +0530

    change portfolio test style

--- js/app.js
+++ js/app.js
@@ -366,3 +366,18 @@ function helloWorld(elm) {
     elm.innerText = "JavaScript";
   }, 5000);
 }
+
+let spans = document.querySelectorAll('.word span');
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
